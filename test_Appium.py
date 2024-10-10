8cf244b5fe7dbe85745d15790388cfbc02dd1474
AndrewQT <andrewnguyen1898@gmail.com>
2024-10-10 10:51:39 +0700

    create define login

--- test_Appium.py
+++ test_Appium.py
@@ -7,37 +7,35 @@ from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
 
 desired_caps = {
-    "platformName": "Android",
-    "platformVersion": "9",
-    "deviceName": "emulator-5554",
-    # "app": "E:\ProjectPython\AutomationMoovtek\Login.apk",
-    "appPackage": "com.moovtek.driver.dev",
-    "appActivity":"com.moovtek.driver.dev.MainActivity",
-    "automationName": "UIautomator2",
-    "autoGrantPermissions": "true"
+  "platformName": "Android",
+  "appium:platformVersion": "14",
+  "appium:deviceName": "OBSWJNR88PZLLZCQ",
+  "appium:appPackage": "com.moovtek.driver.dev",
+  "appium:appActivity": "com.moovtek.driver.dev.MainActivity",
+  "appium:automationName": "UIautomator2",
+  "appium:autoGrantPermissions": "true"
 }
 options = UiAutomator2Options().load_capabilities(desired_caps)
 driver = webdriver.Remote('http://localhost:4723', options=options)
-driver.implicitly_wait(10)
+driver.implicitly_wait(4)
 
-el1 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText")
-el1.send_keys("0999000006")
-el2 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().className(\"com.horcrux.svg.PathView\").instance(223)")
-el2.click()
-el3 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Tiếp tục")
-el3.click()
-el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().resourceId(\"otp_input_0\")")
-el4.send_keys("0")
-el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().resourceId(\"otp_input_1\")")
-el4.send_keys("0")
-el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().resourceId(\"otp_input_2\")")
-el4.send_keys("0")
-el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().resourceId(\"otp_input_3\")")
-el4.send_keys("0")
-el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().resourceId(\"otp_input_4\")")
-el4.send_keys("0")
-el4 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().resourceId(\"otp_input_5\")")
-el4.send_keys("0")
-el1 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.Button")
-el1.click()
-assert driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Bản đồ").is_displayed()
+def test_login():
+    el1 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.EditText")
+    el1.send_keys("0999000006")
+    el2 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR, value="new UiSelector().className(\"com.horcrux.svg.PathView\").instance(223)")
+    el2.click()
+    el3 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Tiếp tục")
+    el3.click()
+
+
+    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="otp_input_0"]').send_keys("0")
+    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="otp_input_1"]').send_keys("0")
+    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="otp_input_2"]').send_keys("0")
+    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="otp_input_3"]').send_keys("0")
+    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="otp_input_4"]').send_keys("0")
+    driver.find_element(by=AppiumBy.XPATH, value='//android.widget.EditText[@resource-id="otp_input_5"]').send_keys("0")
+    el1 = driver.find_element(by=AppiumBy.CLASS_NAME, value="android.widget.Button")
+    el1.click()
+    assert driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Bản đồ").is_displayed()
+    time.sleep(2)
+    driver.quit()
